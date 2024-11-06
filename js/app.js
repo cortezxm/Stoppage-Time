2bcb75a459850fbb9fba2f2b9e1413aad2b7f48a
ArturoLlb25 <arturo03pro@gmail.com>
2024-11-05 22:38:12 -0600

    menu desplegable

---
+++ js/app.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded',function(){
+    eventListeners();
+});
+//Al escuchar el evento ejecuta la funcion (CallBack)
+//Registre un listener que escucha por DomContentLoaded, que el documento cargue tanto el html, js y css
+
+
+function eventListeners(){
+    const barraMovil=document.querySelector('.barra-movil');
+
+    barraMovil.addEventListener('click',navegacionResponsive);
+}
+
+function navegacionResponsive(){
+    console.log('HOLAAAAAAAAA');
+    const navegacion=document.querySelector('.navegacion');
+    //Basicamente al hacer click si contiene mostrar lo elimina, si no lo contiene, lo agrega 
+    if(navegacion.classList.contains('mostrar')){
+        navegacion.classList.remove('mostrar');
+        
+    }else{
+        navegacion.classList.add('mostrar');
+        
+    }
+   //Hace lo mismo pero en una linea jajaja no sabia
+   //navegacion.classList.toggle('mostrar');
+}
